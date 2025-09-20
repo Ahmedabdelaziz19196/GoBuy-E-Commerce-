8a3507a41cc9c8a570a136b152c6f18c0a6783ac
Ahmed abdelaziz <ahmed.abdelaziz19196@gmail.com>
2025-09-20 18:45:08 +0300

    final

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./i18n";
 import { BrowserRouter } from "react-router-dom";
 import { FilterProvider } from "./Context/ProductFilters";
 import { ProductsProvider } from "./Context/TheProducts";
